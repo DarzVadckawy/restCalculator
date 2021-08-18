df02763bcb50e0127ba2bc7334a21e018f54f44c
Dário Silvano Maxaieie <dariomaxaieie@gmail.com>
2021-08-18 17:18:44 +0200

    create the CalculatorService class for the services of the calculator

---
+++ src/main/java/co/mz/restcalculator/service/CalculatorService.java
@@ -0,0 +1,73 @@
+package co.mz.restcalculator.service;
+
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Dário Silvano Maxaieie
+ */
+@Service
+public class CalculatorService {
+
+    private BigDecimal a_value;
+    private BigDecimal b_value;
+    private BigDecimal result;
+
+
+    public String sum(String a, String b){
+
+
+        a_value = new BigDecimal(a);
+
+        b_value = new BigDecimal(b);
+
+        result = a_value.add(b_value);
+
+        return String.valueOf(result);
+
+    }
+
+    public String difference(String a, String b){
+
+
+        a_value = new BigDecimal(a);
+
+        b_value = new BigDecimal(b);
+
+        result = a_value.subtract(b_value);
+
+        return String.valueOf(result);
+
+    }
+
+    public String product(String a, String b){
+
+
+        a_value = new BigDecimal(a);
+
+        b_value = new BigDecimal(b);
+
+        result = a_value.multiply(b_value);
+
+        return String.valueOf(result);
+
+    }
+
+    public String division(String a, String b){
+
+
+        a_value = new BigDecimal(a);
+
+        b_value = new BigDecimal(b);
+
+        result = a_value.divide(b_value);
+
+        return String.valueOf(result);
+
+    }
+
+
+
+}
